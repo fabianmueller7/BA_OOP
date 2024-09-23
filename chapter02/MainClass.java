084b050ed0270bd647977d912b42ef16e07104ec
muellefa <muellerfabian@bluemail.ch>
2024-09-23 16:17:05 +0200

    chapter02 MainClass

--- chapter02/MainClass.java
+++ chapter02/MainClass.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class MainClass {
+    public static void main(String[] args){
+        System.out.println("Start des Programmes");
+
+        Temperatur obj1Temperatur = new Temperatur(20);
+        System.out.println("Celsius: " + obj1Temperatur.getUmgebungstemperaturInCelsius());
+        System.out.println("Kelvin: " + obj1Temperatur.getUmgebungstemperaturInKelvin());
+        System.out.println("Fahrenheit: " + obj1Temperatur.getUmgebungstemperaturInFahrenheit());
+
+
+    }
 }
