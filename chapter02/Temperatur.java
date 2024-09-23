839803bfdd910bb0834f842defcf3231d1414a77
muellefa <muellerfabian@bluemail.ch>
2024-09-23 16:09:52 +0200

    added getter und setter

--- chapter02/Temperatur.java
+++ chapter02/Temperatur.java
@@ -11,15 +11,15 @@ public class Temperatur
 
 
     // instance variables - replace the example below with your own
-    private float UmgebungstemperaturInCelsius;
+    private float UmgebungstemperaturInCelsius = 20;
 
     /**
      * Constructor for objects of class Temperatur
      */
-    public Temperatur()
+    public Temperatur(float startUmgebungstemperaturInCelsius)
     {
         // initialise instance variables
-        this.Umgebungstemperatur = 0.0f;
+        setUmgebungstemperaturInCelsius(startUmgebungstemperaturInCelsius);
     }
 
     /**
@@ -29,9 +29,26 @@ public class Temperatur
      * @return    the sum of x and y
      */
     public float getUmgebungstemperaturInCelsius() {
-        return UmgebungstemperaturInCelsius;
+        return this.UmgebungstemperaturInCelsius;
+    }
+
+    public float getUmgebungstemperaturInKelvin() {
+        return this.UmgebungstemperaturInCelsius + 273.15f;
+    }
+
+    public float getUmgebungstemperaturInFahrenheit() {
+        return this.UmgebungstemperaturInCelsius * 1.8f + 32;
     }
 
     public void setUmgebungstemperaturInCelsius(float umgebungstemperaturInCelsius) {
-        this.UmgebungstemperaturInCelsius = umgebungstemperaturInCelsius;}
+        this.UmgebungstemperaturInCelsius = umgebungstemperaturInCelsius;
+    }
+
+    public void setUmgebungstemperaturInKelvin(float umgebungstemperaturInKelvin)  {
+        this.UmgebungstemperaturInCelsius = umgebungstemperaturInKelvin - 273.15f;
+    }
+
+    public void setUmgebungstemperaturInFahrenheit(float umgebungstemperaturInFahrenheit)  {
+        this.UmgebungstemperaturInCelsius = (umgebungstemperaturInFahrenheit - 32)*(5/9);
+    }
 }
