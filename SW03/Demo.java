8d2d8308c4a82dc11eedf90bf098461d90641b2c
muellefa <muellerfabian@bluemail.ch>
2024-10-04 13:39:00 +0200

    SW03 completed

--- SW03/Demo.java
+++ SW03/Demo.java
@@ -27,10 +27,68 @@ public class Demo {
         if (b < c) {
             temp = c;
         }
+        if (a > c) {
+            temp = a;
+        }
         return temp;
     }
 
     public int max2(int a, int b, int c) {
         return Math.max(Math.max(a,b),c);
+    }
+
+    public static void outputNumbersFromToFOR(int start, int end) {
+        System.out.println("FOR:");
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void outputNumbersFromToWhile(int start, int end) {
+        System.out.println("WHILE:");
+        int i = start;
+        while (i <= end) {
+            System.out.println(i++);
+        }
+    }
+
+    public static void outputNumbersFromToDoWhile(int start, int end) {
+        System.out.println("DoWHILE:");
+        int i = start;
+        do {
+            System.out.println(i++);
+        }while (i <= end);
+    }
+
+    public static void incFloatIncremental(double start, float end, double increment) {
+        System.out.println("FloatIncremental:");
+        for (int i = 1; start <= end; start += increment, i++) {
+            System.out.println(i + " : " + start);
+        }
+        //Number of increments 4000 : 0.999975000000211
+    }
+
+    public static void printBox(final int width, final int height) {
+        if(width < 15 && height < 15) {
+
+            for (int i = 0; i <= height; i++) {
+                for (int j = 0; j <= width; j++) {
+
+                    if(i%height ==0 || j%width == 0) {
+                        System.out.print("#");
+                    } else {
+                        System.out.print(" ");
+                    }
+
+                    if(j == width) {
+                        System.out.println(" ");
+                    }
+
+                }
+            }
+
+        } else {
+            System.out.println("Your Box is too big");
+        }
     }
 }
--- SW03/MainClass.java
+++ SW03/MainClass.java
@@ -4,5 +4,11 @@ public class MainClass {
     public static void main(String[] args){
         System.out.println("Start des Programmes");
 
+        //Demo.outputNumbersFromToFOR(0,10);
+        //Demo.outputNumbersFromToWhile(0,10);
+        //Demo.outputNumbersFromToDoWhile(0,10);
+        //Demo.incFloatIncremental(0.9d, 1.0f, 0.000025d);
+
+        Demo.printBox(10,5);
     }
 }
