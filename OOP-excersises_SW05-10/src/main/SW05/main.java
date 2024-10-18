559467d7b9204f697b089f6533c03a5d49863469
muellefa <muellerfabian@bluemail.ch>
2024-10-18 14:56:50 +0200

    main.java SW05

--- OOP-excersises_SW05-10/src/main/SW05/main.java
+++ OOP-excersises_SW05-10/src/main/SW05/main.java
@@ -2,5 +2,19 @@ public class main {
 
     public static void main(String[] args) {
 
+        System.out.println("SW05...");
+
+        Rectangle rectangleObj = new Rectangle(0,0,5,10);
+        System.out.println("Rectangle Area: " + rectangleObj.getArea());
+        System.out.println("Rectangle Perimeter: " + rectangleObj.getPerimeter());
+
+        Square squareObj = new Square(0,0,10);
+        System.out.println("Square Area: " + squareObj.getArea());
+        System.out.println("Square Perimeter: " + squareObj.getPerimeter());
+
+        Circile circileObj = new Circile(0,0,10);
+        System.out.println("Circle Area: " + circileObj.getArea());
+        System.out.println("Circle Perimeter: " + circileObj.getPerimeter());
+
     }
 }
