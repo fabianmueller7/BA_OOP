45c7c501a4774506747fe929820ef11a0f337b7b
muellefa <muellerfabian@bluemail.ch>
2024-10-25 11:16:13 +0200

    fixed SW05 Named class

--- OOP-excersises_SW05-10/src/main/SW05/main.java
+++ OOP-excersises_SW05-10/src/main/SW05/main.java
@@ -4,17 +4,26 @@ public class main {
 
         System.out.println("SW05...");
 
-        Rectangle rectangleObj = new Rectangle(0,0,5,10);
-        System.out.println("Rectangle Area: " + rectangleObj.getArea());
-        System.out.println("Rectangle Perimeter: " + rectangleObj.getPerimeter());
+        Rectangle rectangleObj = new Rectangle(0,0, "Rectangle",5,10);
+        System.out.println(rectangleObj.getName());
+        System.out.println("Area: " + rectangleObj.getArea());
+        System.out.println("Perimeter: " + rectangleObj.getPerimeter());
 
-        Square squareObj = new Square(0,0,10);
-        System.out.println("Square Area: " + squareObj.getArea());
-        System.out.println("Square Perimeter: " + squareObj.getPerimeter());
+        Square squareObj = new Square(0,0,"Square" ,10);
+        System.out.println(squareObj.getName());
+        System.out.println("Area: " + squareObj.getArea());
+        System.out.println("Perimeter: " + squareObj.getPerimeter());
 
-        Circile circileObj = new Circile(0,0,10);
-        System.out.println("Circle Area: " + circileObj.getArea());
-        System.out.println("Circle Perimeter: " + circileObj.getPerimeter());
+        Circile circileObj = new Circile(0,0, "Circle",10);
+        System.out.println(circileObj.getName());
+        System.out.println("Area: " + circileObj.getArea());
+        System.out.println("Perimeter: " + circileObj.getPerimeter());
+
+        CountingSwitchable countingSwitchableObj = new CountingSwitchable();
+        countingSwitchableObj.switchOn();
+        countingSwitchableObj.switchOff();
+        countingSwitchableObj.switchOn();
+        System.out.println("SwitchCounter: " + countingSwitchableObj.getSwitchCounter());
 
     }
 }
