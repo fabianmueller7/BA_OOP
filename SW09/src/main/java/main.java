6c44ab39e57000d92af673cf0c81af4bf4201060
muellefa <muellerfabian@bluemail.ch>
2024-11-11 17:27:40 +0100

    SW09 Abschnitt 3

--- SW09/src/main/java/main.java
+++ SW09/src/main/java/main.java
@@ -1,27 +1,34 @@
 import java.util.Scanner;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class main {
 
     public static void main(String[] args) {
         String input;
         Scanner scanner = new Scanner(System.in);
+
+        final Logger LOG = LoggerFactory.getLogger(main.class);
+
         do {
             System.out.println("Bitte Temperatur eingeben (oder 'exit' zum Beenden): ");
+            LOG.info("Getting input from user.");
             input = scanner.next();
             try{
                 float value = Float.valueOf(input);
                 System.out.println("Eingegebene Zahl: " + value);
             } catch (Exception e) {
 
+
                 if(input.equals("exit")) {
                     break;
                 }
 
-                System.out.println("Bitte geben Sie eine Zahl eingeben.");
-                System.out.println(e.getMessage());
+                LOG.warn(e.getMessage());
+                LOG.error("Bitte geben Sie eine Zahl eingeben.");
             }
 
         } while (!"exit".equals(input));
-        System.out.println("Programm beendet.");
+        LOG.info("Programm beendet.");
     }
 }
